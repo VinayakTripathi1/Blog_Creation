d7218e96570d08a66e4960fb46502aee7861ae6c
Vinayak Tripathi <tripathivinayak29@gmail.com>
2024-03-14 17:32:15 +0530

    First Commit

---
+++ myapp/src/scroller.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function horizontalscroller() {
+  return (
+    <>  
+    <div id="vin" class="carousel slide" data-ride="carousel">
+<ol className="carousel-indicators">
+    <li data-target="#vin" data-slide-to="0" class="active"></li>
+    
+</ol>
+
+    </div>
+    </>
+  
+  )
+}
